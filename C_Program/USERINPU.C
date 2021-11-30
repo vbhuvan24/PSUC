027d3dfef1e5688be2b9f52bc67579d5e4e7e94d
Yash Verma <56463836+vbhuvan24@users.noreply.github.com>
2021-11-30 22:30:07 +0530

    Add files via upload

---
+++ C_Program/USERINPU.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main ()
+{
+int a,b;
+float c;
+
+clrscr();
+
+printf("Enter first number N1 :");
+scanf("%d",&a);
+
+printf("Enter Second number N2 :");
+scanf("%d",&b);
+
+c = a + b ;
+
+printf("Sum of Two Number : %f",c);
+
+getch();
+}
