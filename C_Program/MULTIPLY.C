882c3037f71be5b2a394f3b5f5aa6980a1624dc3
Yash Verma <56463836+vbhuvan24@users.noreply.github.com>
2021-12-01 19:57:38 +0530

    Add files via upload

---
+++ C_Program/MULTIPLY.C
@@ -0,0 +1,20 @@
+
+x#include<stdio.h>
+
+void main()
+{
+float a,b,c;
+clrscr();
+
+printf("Enter the First Number N1 :");
+scanf("%f",a);
+
+printf("Enter the Second Number N2 :");
+scanf("%f",b);
+
+c = a * b ;
+
+printf(" Multiple Value is : %f ",c);
+
+getch();
+}
