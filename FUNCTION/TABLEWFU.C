370c64fa1671b9446877fae28f8ae25f87a62a52
Yash Verma <56463836+vbhuvan24@users.noreply.github.com>
2022-01-01 22:00:11 +0900

    Add files via upload

---
+++ FUNCTION/TABLEWFU.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void table(int);
+
+int main()
+{
+int num;
+printf("Enter a positve number\n");
+scanf("%d",&num);
+printf("\n multiplication Table for %d is :\n",num);
+
+table(num);
+
+return 0;
+}
+void table (int num)
+{
+int count ;
+
+for(count=1;count <= 10;count++)
+{
+printf("%d x%d=%d\n",num,count ,num*count);
+}
+}
