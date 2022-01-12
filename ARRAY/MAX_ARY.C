76f9094b827fdb98b1408780e435428478be1438
Yash Verma <56463836+vbhuvan24@users.noreply.github.com>
2022-01-12 15:31:58 +0530

    Add files via upload

---
+++ ARRAY/MAX_ARY.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+	int ary[6];
+	int i,max;
+	clrscr();
+
+printf("Enter the element of Array :\n");
+//Input Array Loop
+for(i=0;i<6;i++){
+	scanf("%d",&ary[i]);
+	}
+//Checking For Maximum Number
+for(i=0;i<6;i++){
+	if(max < ary[i]){
+		max = ary[i];
+		}
+	}
+printf("Greatest element of array = %d",ary[i]);
+getch();
+}
